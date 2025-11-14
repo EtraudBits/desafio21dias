492ed34ed84e5b92f79cbf689f81783feeb94113
Duarte <du84arte@gmail.com>
2025-11-13 21:15:57 -0300

    missão dia3

--- dia3.go
+++ dia3.go
@@ -1,31 +1,30 @@
 package main
 
-import "fmt" //pacote para imprimir e ler dados
+import "fmt" // pacote para imprimir e ler dados
 
-//Missão do dia 3 do desafio, estudar variaveis, tipos de dados e operadores
-// objetivo criar um script simples que receba entradas e exiba resultados
+//missão dia 3 - estudar variaveis, tipo de dados e operadores
+//objetivo criar um script simples que receba entrada e exiba resultado
 
-func main() {
+func main ()  {
 
 	//variaveis simples
 	var nome string
 	var idade int
 
-	fmt.Println("Digite seu nome:") //Pergunta para o usuario 
-	fmt.Scanln(&nome) //>Scanln< ler o que o usuario digitou
+	fmt.Println("Digite seu nome:") //exibe na tela para o usuario
+	fmt.Scanln(&nome) // >Scanln< serve para ler o que o usuario digitou
 
-	fmt.Println("Digite sua Idade")
+	fmt.Println("Digite sua idade:")
 	fmt.Scanln(&idade)
 
-	//usando operadores
-	//vamos somar quantos anos ele terá em determinado tempo
+	//usando agora operadores
 
-	idadeFutura := idade + 1 //somará a idade + 1
+	tempoDev := idade + 1
 
-	//vamos imprimir o resultado
+	//exibi o resultado
 	fmt.Println("Olá,", nome, ".")
-	fmt.Println("Você será um DEV com", idadeFutura, "anos.")
+	fmt.Println("Você será um DEV com,", tempoDev, "anos")
 
-	fmt.Println("Vamos, continue tento foco,", nome, ". Você conseguirá.")
-
+	fmt.Println("Continue com foco, ",nome, ", você vai conseguir!")
+	
 }
